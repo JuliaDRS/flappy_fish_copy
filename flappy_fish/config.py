8cd0ff0169d66244ae64031d1e3a48e0eadbf4df
julia Di Russo <julia.di.russo@sogeti.com>
2019-11-26 19:39:11 +0100

    First commit

---
+++ flappy_fish/config.py
@@ -0,0 +1,21 @@
+"""config.py
+
+Contains all global variables for flappy_bird
+"""
+
+# Sizes
+WIDTH = 400
+HEIGHT = 600
+# fish
+CIRCLE_RADIUS = 30
+FISH_NR = 30
+# Pipes
+PIPE_WIDTH = 50
+HALF_GAP_SIZE = 80
+PIPE_MARGIN = 50
+
+# Physics
+GRAVITY = 0.7
+BIRD_MASS = 1
+JUMP_FORCE = -15
+PIPE_SPEED = 2.0
